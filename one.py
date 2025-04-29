32866cd22802e09c2bdc08c18d8c34726b3041f7
Rainnn418 <xxu@putnamscience.org>
2025-04-29 16:32:02 -0400

    Update one.py

--- one.py
+++ one.py
@@ -6,16 +6,21 @@ class books():
         self.Books = {}
         
     def Add(self):
-        self.title = input("Enter the name of the book you add    \n")
-        self.author = input("Enter the Author of the book you add    \n")
-        self.price = float(input("Enter the listing price of the book    \n"))
-        self.Books[self.title] = {'author': self.author, 'price': self.price}
-       
-        print("You have added", self.title)
+        try:
+            self.title = input("Enter the name of the book you add    \n")
+            self.author = input("Enter the Author of the book you add    \n")
+            self.price = float(input("Enter the listing price of the book    \n"))
+            self.Books[self.title] = {'author': self.author, 'price': self.price}
+            print("You have added", self.title)
+        except ValueError:
+            print("Please enter a number")
         print(self.Books)
 
     def Minus(self):
         self.title = input("Enter the name of the book you remove    \n")
-        del self.Books[self.title]
+        try:
+            del self.Books[self.title]
+        except KeyError:
+            print("The book was not found.")
         print(self.Books)
  
