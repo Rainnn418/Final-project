702ae31aaeb6eff61f930cf908a41b9c69c611fc
px1208 <px12086@gmail.com>
2025-04-29 13:59:12 -0400

    4.29
    4.29

--- one.py
+++ one.py
@@ -10,6 +10,12 @@ class books():
         self.author = input("Enter the Author of the book you add    \n")
         self.price = float(input("Enter the listing price of the book    \n"))
         self.Books[self.title] = {'author': self.author, 'price': self.price}
-
+       
         print("You have added", self.title)
         print(self.Books)
+
+    def Minus(self):
+        self.title = input("Enter the name of the book you remove    \n")
+        del self.Books[self.title]
+        print(self.Books)
+ 
--- two.py
+++ two.py
@@ -1,12 +1,10 @@
 from one import books
 a = books()
+game = True 
+while game:
+    v = input("Would you like to add a book? (yes or no)")
 
-v = input("Would you like to add a book? (yes or no)")
-
-if v == "yes":
-    b = a.Add()
-    b = a.Add()
-elif v == "no":
-    print("You have to Yes")
-    b = a.Add()
-    b = a.Add()
+    if v == "yes":
+        b = a.Add()
+    elif v == "no":
+        c = a.Minus()
